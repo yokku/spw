94ba38639276c43bea245ebde5ae464c717ee619
yokku <yok_Ldm1819@hotmail.com>
2015-03-10 22:53:16 +0700

    Update SpaceShip.java

--- f2/spw/SpaceShip.java
+++ f2/spw/SpaceShip.java
@@ -14,7 +14,7 @@ public class SpaceShip extends Sprite{
 
 	@Override
 	public void draw(Graphics2D g) {
-		g.setColor(Color.GREEN);
+		g.setColor(Color.BLUE);
 		g.fillRect(x, y, width, height);
 		
 	}

d6c3fab7aedbb9cef4898c7993a7736646426212
yokku <yok_Ldm1819@hotmail.com>
2015-03-10 22:50:21 +0700

    Update GamePanel.java

--- f2/spw/GamePanel.java
+++ f2/spw/GamePanel.java
@@ -23,7 +23,7 @@ public class GamePanel extends JPanel {
 	public void updateGameUI(GameReporter reporter){
 		big.clearRect(0, 0, 400, 600);
 		
-		big.setColor(Color.WHITE);		
+		big.setColor(Color.GREEN);		
 		big.drawString(String.format("%08d", reporter.getScore()), 300, 20);
 		for(Sprite s : sprites){
 			s.draw(big);

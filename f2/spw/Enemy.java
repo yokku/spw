532d25ebb8d9b23130b6e842d77b3f8ddbe20878
yokku <yok_Ldm1819@hotmail.com>
2015-04-08 00:02:37 +0700

    *01*

--- f2/spw/Enemy.java
+++ f2/spw/Enemy.java
@@ -5,14 +5,14 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 
 public class Enemy extends Sprite{
-	public static final int Y_TO_FADE = 400;
+	public static final int Y_TO_FADE = 800;
 	public static final int Y_TO_DIE = 600;
 	
 	private int step = 12;
 	private boolean alive = true;
 	
 	public Enemy(int x, int y) {
-		super(x, y, 5, 10);
+		super(x, y, 15, 10);
 		
 	}
 

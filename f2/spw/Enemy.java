6ce5e2af2c8b33af266dbeb2c1ce92c6c7857c02
yokku <yok_Ldm1819@hotmail.com>
2015-04-12 00:20:52 +0700

    *02*

--- f2/spw/Enemy.java
+++ f2/spw/Enemy.java
@@ -5,21 +5,21 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 
 public class Enemy extends Sprite{
-	public static final int Y_TO_FADE = 800;
+	public static final int Y_TO_FADE = 400;
 	public static final int Y_TO_DIE = 600;
 	
-	private int step = 12;
+	private int step = 15;
 	private boolean alive = true;
 	
 	public Enemy(int x, int y) {
-		super(x, y, 15, 10);
+		super(x, y, 5, 30);
 		
 	}
 
 	@Override
 	public void draw(Graphics2D g) {
 		if(y < Y_TO_FADE)
-			g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f));
+			g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.9f));
 		else{
 			g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 
 					(float)(Y_TO_DIE - y)/(Y_TO_DIE - Y_TO_FADE)));

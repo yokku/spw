18fd33c4124dd64a18330cdd0efdb5bea371406e
yokku <yok_Ldm1819@hotmail.com>
2015-04-18 23:54:22 +0700

    *04*  add image enemy,spaceship,item,add bullet
    -เพิ่มรูปภาพให้enemy,spaceship,item
    -เพิ่มให้ยานสามารถยิงได้
    -เปลี่ยนตำแหน่งเริ่มต้นของยาน ปรับความเร็วและจำนวนของศัตรู

--- f2/spw/SecondEnemy.java
+++ f2/spw/SecondEnemy.java
@@ -4,16 +4,28 @@ import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 public class SecondEnemy extends Sprite{
 	public static final int Y_TO_FADE = 400;
 	public static final int Y_TO_DIE = 600;
 	
 	private int step = 10;
 	private boolean alive = true;
-	
+	BufferedImage image;
+
 	public SecondEnemy(int x, int y) {
-		super(x, y, 25, 30);
+		super(x, y, 50, 50);
 		
+		try{
+			image = ImageIO.read(new File("f2/image/SecondEnemy.png"));
+		}
+		catch(IOException e){
+
+		}
 	}
 
 	@Override
@@ -24,9 +36,9 @@ public class SecondEnemy extends Sprite{
 			g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 
 					(float)(Y_TO_DIE - y)/(Y_TO_DIE - Y_TO_FADE)));
 		}
-		g.setColor(Color.PINK);
-		g.fillRect(x, y, width, height);
-		
+		//g.setColor(Color.PINK);
+		//g.fillRect(x, y, width, height);
+		g.drawImage(image, x, y, width, height, null);
 	}
 
 	public void proceed(){

eaaa7c39f7d33d27d0c8ee92ebccebacdd7364b6
yokku <yok_Ldm1819@hotmail.com>
2015-04-18 23:53:42 +0700

    *04*  add image enemy,spaceship,item,add bullet
    -เพิ่มรูปภาพให้enemy,spaceship,item
    -เพิ่มให้ยานสามารถยิงได้
    -เปลี่ยนตำแหน่งเริ่มต้นของยาน ปรับความเร็วและจำนวนของศัตรู

--- f2/spw/Item.java
+++ f2/spw/Item.java
@@ -3,17 +3,26 @@ package f2.spw;
 import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Graphics2D;
-
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 public class Item extends Sprite{
 	public static final int Y_TO_FADE = 400;
 	public static final int Y_TO_DIE = 600;
 	
 	private int step = 15;
 	private boolean alive = true;
-	
+	BufferedImage image;
+
 	public Item(int x, int y) {
-		super(x, y, 10, 10);
-		
+		super(x, y, 15, 15);
+		try{
+			image = ImageIO.read(new File("f2/image/Item.png"));
+		}
+		catch(IOException e){
+
+		}
 	}
 
 	@Override
@@ -25,9 +34,9 @@ public class Item extends Sprite{
 					(float)(Y_TO_DIE - y)/(Y_TO_DIE - Y_TO_FADE)));
 		}
 
-		g.setColor(Color.RED);
-		g.fillRect(x, y, width, height);
-		
+		//g.setColor(Color.RED);
+		//g.fillRect(x, y, width, height);
+		g.drawImage(image, x, y, width, height, null);
 	}
 
 	public void proceed(){
